61e73d3f488e71774afc81c66013b52025524bc6
m.shepherd <55757863+shepherdm1@users.noreply.github.com>
2020-10-24 15:27:41 -0400

    Getting JFX working. TXT with VM arguments stored in SRC folder

--- src/edu/wit/shepherdm1dyern1/p2pchess/Main.java
+++ src/edu/wit/shepherdm1dyern1/p2pchess/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".\nThere's a text file with vm arguments in the src folder.");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
