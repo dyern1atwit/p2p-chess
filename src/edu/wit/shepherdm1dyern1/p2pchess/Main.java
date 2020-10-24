5650d1703e40ff5a749e3e8f390fd5c06163aab0
m.shepherd <55757863+shepherdm1@users.noreply.github.com>
2020-10-24 15:26:33 -0400

    Testing to make sure shepherdm1 can commit properly.

--- src/edu/wit/shepherdm1dyern1/p2pchess/Main.java
+++ src/edu/wit/shepherdm1dyern1/p2pchess/Main.java
@@ -1,7 +1,24 @@
 package edu.wit.shepherdm1dyern1.p2pchess;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
-public class Main {
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("michael commit test");
+        launch(args);
     }
 }
