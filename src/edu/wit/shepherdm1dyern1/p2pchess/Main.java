4f2cb230deddf04a28c5e4a41287e4815896c772
m.shepherd <55757863+shepherdm1@users.noreply.github.com>
2020-10-24 14:52:53 -0400

    Testing to make sure shepherdm1 can commit properly.

--- src/edu/wit/shepherdm1dyern1/p2pchess/Main.java
+++ src/edu/wit/shepherdm1dyern1/p2pchess/Main.java
@@ -2,6 +2,6 @@ package edu.wit.shepherdm1dyern1.p2pchess;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("michael commit test");
     }
 }
